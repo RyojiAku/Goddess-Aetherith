fa7565dd280041241cc562379ac0012f55eb24ba
RyojiAku <YiAkuma@web.de>
2017-12-31 20:15:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!')
-    bot.user.setPresence({ game: { name: 'hello', type: 0 } });
+    client.user.setPresence({ game: { name: 'hello', type: 0 } });
 });
 
 client.on('message', message => {
